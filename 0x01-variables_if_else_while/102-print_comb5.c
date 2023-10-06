8ea5f8bd9393154870cd613ddd5c9c32f172902b
akoredera <akoredera@gmail.com>
2023-10-06 13:46:11 -0700

    Add 102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			for (k = i; k < 10; k++)
+			{
+				for (l = j; l < 10; l++)
+				{
+					if ((i == 0 && j == 0) && (k == 0 && l == 0))
+						continue;
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if ((i == 9 && j == 8) && (k == 9 && l == 9))
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
