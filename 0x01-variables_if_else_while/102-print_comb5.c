ce9b1c3ed00e35cfeb6c87b1f1f9f6559d767dc4
akoredera <akoredera@gmail.com>
2023-10-06 14:49:19 -0700

    Modified 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (k = j; k < 10; k++)
+			for (k = 0; k < 10; k++)
 			{
-				for (l = 0; l < 10; l++)
+				for (l = 1; l < 10; l++)
 				{
 					if ((i == 0 && j == 0) && (k == 0 && l == 0))
 						continue;
