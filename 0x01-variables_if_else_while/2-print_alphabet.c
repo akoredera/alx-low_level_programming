6287bc62aa8de8c6bce0684b8efcb51184ebcaac
akoredera <akoredera@gmail.com>
2023-10-06 07:05:20 -0700

    Add file 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, c;
+
+	for (n = 0; n <= 26; n++)
+		putchar(c);
+	return (0);
+}
