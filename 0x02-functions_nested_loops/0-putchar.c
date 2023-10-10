5ce401e28e5259d0ad76f4f766638945ae79e9e4
akoredera <akoredera@gmail.com>
2023-10-10 08:54:14 -0700

    Add 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 /**
- * main | entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * return (0) success
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+ * main | Entry point
+ *
+ * return: Always 0 (success)
  */
 
-int _putchar(char c);
-
-int main (void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
