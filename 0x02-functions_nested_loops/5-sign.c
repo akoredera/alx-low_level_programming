607fb1364b233c5bc8b71fbb0f7568c5c396fd3c
akoredera <akoredera@gmail.com>
2023-10-10 12:51:34 -0700

    Add 5-sign.c file and modified main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - return 1 or 0 or -1 for positive or zero
+ * or negative value respectively
+ * @n: The number to print;
+ *
+ * Return: On positve 1
+ * Otherwise: 0 on zero
+ * lastly: -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+	}
+	return (-1)
+}
