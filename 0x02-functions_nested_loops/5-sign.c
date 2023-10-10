41decca646b10afba3049a875bfbea34d0b4a115
akoredera <akoredera@gmail.com>
2023-10-10 13:24:10 -0700

    modified 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,11 +13,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
+		_putchar(43);
+		(return (1);
 	}
 	else if (n == 0)
 	{
 		return (0);
 	}
-	return (-1);
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
 }
