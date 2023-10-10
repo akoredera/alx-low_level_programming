ddc056db5e7cc51b7efc15f9d8325012ee19a49e
akoredera <akoredera@gmail.com>
2023-10-10 16:17:58 -0700

    modify 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,13 +16,20 @@ void times_table(void)
 			{
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
+				if (b == 9)
+					break;
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
+			{
 				_putchar(c + '0');
-			if (b == 9)
-				break;
-			_putchar(',');
-			_putchar(' ');
+				if (b == 9)
+					break;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
