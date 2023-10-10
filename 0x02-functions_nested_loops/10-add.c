2471ca703304dc8e30e7be025bfbbd27e7ff9fb9
akoredera <akoredera@gmail.com>
2023-10-10 16:44:51 -0700

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+ * add - addition of two digits
+ * @a: value to add
+ * @b: value to add
+ *
+ * Return: c as the output
+ */
+#include "main.h"
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
