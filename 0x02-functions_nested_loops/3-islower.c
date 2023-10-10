ef2a522c23df6aedeb79c125ae4f765333285ab4
akoredera <akoredera@gmail.com>
2023-10-10 11:24:40 -0700

    Add 3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include <ctype.h>
+
+/**
+ * _islower - return 1 or 0
+ * @c: The character to print;
+ *
+ * Return: On Success 1
+ * Otherwise: 0 is returned
+ */
+int _islower(int c)
+{
+	if (int islower(int c))
+		return (1);
+	else
+		return (0);
