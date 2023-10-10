e07f5b0325efa9932b6c42697df2e3dde8d59336
akoredera <akoredera@gmail.com>
2023-10-10 15:15:31 -0700

    Add file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,21 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
         {
-                for (j = 0; j <= 5; j++)
+                for (j = 0; j <= 9; j++)
                 {
-                        for (k = 0; k <= 5; k++)
-                        {
-                                for (l = 0; l <= 9; l++)
-                                {
-                                        _putchar(i + '0');
-                                        _putchar(j + '0');
-                                        _putchar(':');
-                                        _putchar(k + '0');
-                                        _putchar(l + '0');
-					_putchar('\n');
+			if ((i <= 2 && j <= 3) || (i <= 1 && j <= 9))
+			{
+	                        for (k = 0; k <= 5; k++)
+        	                {
+                	                for (l = 0; l <= 9; l++)
+                        	        {
+                                	        _putchar(i + '0');
+                                	        _putchar(j + '0');
+                                        	_putchar(':');
+                                        	_putchar(k + '0');
+                                        	_putchar(l + '0');
+						_putchar('\n');
+					}
                                 }                     
                         }
                 }
