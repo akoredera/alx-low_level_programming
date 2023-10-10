b030ccd46afe8fd971dafbe87c77c783e9244acd
akoredera <akoredera@gmail.com>
2023-10-10 10:28:58 -0700

    Add newline to 2-print_alphabet_x10 file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,8 @@ void print_alphabet_x10(void)
 	char letter;
 	int t;
 
-	for (t = 0; t < 10; t++){
+	for (t = 0; t < 10; t++)
+	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
