7afa5662b9c9f86dcd1a1ea0033877f750c10de4
akoredera <akoredera@gmail.com>
2023-10-11 13:34:55 -0700

    modified 3-print_remaining_days.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+void times_table(void)
+{
+        int a, b, c;
+
+        for (a = 0; a < 10; a++)
+        {
+                for (b = 0; b < 10; b++)
+                {
+                        c = a * b;
+                        if (c >= 10 && c <= 90)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(c / 10 + '0');
+                                _putchar(c % 10 + '0');
+                        }
+                        else
+                        {
+                                if (b != 0)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                }
+                                _putchar(c + '0');
+                        }
+                }
+                _putchar('\n');
+        }
+}
