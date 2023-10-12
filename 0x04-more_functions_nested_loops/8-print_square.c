72e03dc3048d6348c50b75d3f35c798af5ee97bb
akoredera <akoredera@gmail.com>
2023-10-12 06:38:27 -0700

    Add 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: number that determine square size
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
