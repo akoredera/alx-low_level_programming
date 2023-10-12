cf1590f68968631bf35ce37f7e581c026d8b4597
akoredera <akoredera@gmail.com>
2023-10-12 00:01:55 -0700

    Add 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "ctype.h"
+
+/**
+ * _isupper - check for Capital letter
+ * @c: character to check
+ *
+ * Return 1 success
+ * otherwise 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
