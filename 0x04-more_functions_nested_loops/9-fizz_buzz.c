5dbd34658d3d77930d140a78b7156fc6d2c86d1b
akoredera <akoredera@gmail.com>
2023-10-12 10:25:17 -0700

    update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,37 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
-			printf("Fizz ");
+		if (a == 100)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (a % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
 		else if (a % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			printf(" ");
+		}
 		else if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
 		else
-			printf("%d ", a);
+		{
+			printf("%d", a);
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
