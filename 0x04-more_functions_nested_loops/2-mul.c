2a13d37eed3d012be8b07266983e7ba49a7b9c05
akoredera <akoredera@gmail.com>
2023-10-12 03:25:28 -0700

    Add 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "ctype.h"
+
+/**
+ * mul - multiply two digit
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result through c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
