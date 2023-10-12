d61bee9108270182e22bd29558ee635a5b509c23
akoredera <akoredera@gmail.com>
2023-10-12 08:34:27 -0700

    update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,18 @@
  */
 void more_numbers(void)
 {
-	int a;
+	int a, b;
 
-	for (a = 0; a <= 14; a++)
+	for (b = 0; b < 10; b++)
 	{
-		if (a >= 10)
+		for (a = 0; a <= 14; a++)
 		{
-			_putchar(a / 10 + '0');
+			if (a >= 10)
+			{
+				_putchar(a / 10 + '0');
+			}
 			_putchar(a % 10 + '0');
 		}
-		else
-			_putchar(a + '0');
+		_putchar('\n');
 	}
-	/*_putchar('\n');*/
 }
