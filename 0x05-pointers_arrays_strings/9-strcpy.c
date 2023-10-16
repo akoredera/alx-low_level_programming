cc1960a8173395ea1de8ab2e88571e2521145cce
akoredera <akoredera@gmail.com>
2023-10-16 12:32:50 -0700

    update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,12 @@
 #include "string.h"
+/**
+ * _strcpy - copy string from one string to other
+ * @src: source to copy
+ * @dest: location to copy into
+ *
+ * Return: dest variable value
+ */
 char *_strcpy(char *dest, char *src)
 {
-	return(strcpy(*dest, *src));
+	return (strcpy(dest, src));
 }
