f8cd166a1bf544be8a820b3c5370526ac1c61cda
akoredera <akoredera@gmail.com>
2023-10-16 13:02:04 -0700

    Add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+/**
+ * puts_half - print half of the str provided
+ * @str: str variable to print half of it
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = (strlen(str) - 1) / 2; i < strlen(str); i++)
+		printf("%d", str[i]);
+	printf("\n");
+}
