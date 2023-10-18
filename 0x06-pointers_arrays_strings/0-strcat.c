e762ff3b718b07da86b09332220ea2c1b96fc981
akoredera <akoredera@gmail.com>
2023-10-18 12:28:14 -0700

    Add 0-strcat.c:

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/**
+ * _strcat - concatenate two strings
+ * @dest: first string variable
+ * @src: second string variable
+ * * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, strlen = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		strlen++;
+	}
+	for (j = 0; j < strlen; j++)
+	{
+		dest[strlen + j] = src[j];
+	}
+	return (dest);
+}
