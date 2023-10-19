343905152ccc6c0c3e0c5d1620074457af4aef47
akoredera <akoredera@gmail.com>
2023-10-19 10:29:10 -0700

    create 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
