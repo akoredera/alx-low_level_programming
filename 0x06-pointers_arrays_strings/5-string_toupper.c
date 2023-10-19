d2a3456fb905616885733ef811a5b496a90e01a7
akoredera <akoredera@gmail.com>
2023-10-19 12:54:11 -0700

    Create 5-string_toupper.c file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper - change string to uppercase
+ * @s: string
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
