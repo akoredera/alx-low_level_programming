e8948ddbda6c5ce868d75853784e552688f39d54
akoredera <akoredera@gmail.com>
2023-10-19 15:45:14 -0700

    create 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encodes string function
+ * @a: string to encode
+ * Return: a
+ */
+char *rot13(char *a)
+{
+	int i, j;
+	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
+	char num[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; alp[j] != '\0'; j++)
+		{
+			if (a[i] == alp[j])
+			{
+				a[i] = num[j];
+				break;
+			}
+		}
+	}
+	return (a);
+}
