541f857b0fde8b54a95bc2754b767b9d35a0b1d5
akoredera <akoredera@gmail.com>
2023-10-23 16:05:36 -0700

    Create 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+/**
+ * _strstr -  locates a substring
+ * @haystack: string to check
+ * @needle: locate string
+ * Return: s
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
