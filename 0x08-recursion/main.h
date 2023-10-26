50d594e5c4dda811e25451e27369243fd7d8ffd0
akoredera <akoredera@gmail.com>
2023-10-26 15:39:54 -0700

    updated main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,9 @@ int factorial(int n);
 int _sqrt_recursion(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int len(char* a);
+int comp(char *s, int i, int len);
 
 #endif
