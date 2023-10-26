0644e635d73c6df923904efd633b9f45825beeec
akoredera <akoredera@gmail.com>
2023-10-26 16:02:32 -0700

    push 101-wildcmp.c file

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+/**
+ * wildcmp - compares two strings and returns 1 or 0
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: 0 or 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
