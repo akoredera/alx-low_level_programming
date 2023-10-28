91a451df63e154093f1e1cbed9abb05d4a07ab57
akoredera <akoredera@gmail.com>
2023-10-27 21:23:58 -0700

    push 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - writes its name
+ * @argc: argument count
+ * @argv: array of pointers to argument strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
