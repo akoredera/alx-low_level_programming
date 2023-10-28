e75eb0d5fdeec654c416c75e9f2177a01635eb13
akoredera <akoredera@gmail.com>
2023-10-27 21:31:47 -0700

    pushed 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+/**
+ * main - writes its name
+ * @argc: argument count
+ * @argv: array of pointers to argument strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b res;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	res = a * b;
+	printf("%d\n", res);
+	return (0);
+}
