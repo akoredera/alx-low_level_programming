b8d897a0d393de2686f6476cb372b4c66180994d
akoredera <akoredera@gmail.com>
2023-10-31 09:09:24 -0700

    Add if conditional statement to return NULL for ptr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		if ((i + 1) == size)
