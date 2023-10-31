d5a5cbcd7419a995337886ea6e2d849166acccd9
akoredera <akoredera@gmail.com>
2023-10-31 02:37:55 -0700

    push 1-strdup.c file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "string.h"
+#include "stdlib.h"
+#include <stddef.h>
+/**
+ * _strdup - returns a pointer to a new string which duplicates
+ * @str: string to duplicate
+ * Return: NULL if size = 0; otherwise ptr
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, size;
+
+	if (str == NULL)
+		return (NULL);
+	size = strlen(str) + 1;
+	ptr = malloc(size * sizeof(char));
+	for (i = 0; i <= size; i++)
+	{
+		if (i == size)
+			ptr[i] = '\0';
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
