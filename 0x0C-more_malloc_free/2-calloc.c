d5f9640b427ac5204c04bd2e16127fd8700c6cca
akoredera <akoredera@gmail.com>
2023-11-02 13:11:05 -0700

    push  2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: member of array
+ * @size: memory size
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
+	ptr = calloc(nmemb, size);
+	return (ptr);
+}
