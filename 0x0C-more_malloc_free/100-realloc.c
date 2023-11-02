1b439430195e2e2ab75d2fea9cf3fb324b925b4e
akoredera <akoredera@gmail.com>
2023-11-02 16:19:32 -0700

    modified content

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 	{
+		ptr = malloc(new_size);
 		return (ptr);
 	}
 	if (new_size < old_size)
