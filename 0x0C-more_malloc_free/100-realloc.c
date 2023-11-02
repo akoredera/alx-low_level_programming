2891e0f6c2360248080816f37b25a189b97bdccc
akoredera <akoredera@gmail.com>
2023-11-02 15:44:16 -0700

    push 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "stdlib.h"
+/**
+ * _realloc -  reallocates a memory block
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: the size, in bytes, of the allocated space
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size < old_size)
+	{
+		new_size = old_size;
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	return (ptr);
+}
