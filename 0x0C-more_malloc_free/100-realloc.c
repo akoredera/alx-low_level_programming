201875eda4a4a866321000aaa788b0ba0d2886c7
akoredera <akoredera@gmail.com>
2023-11-02 16:15:12 -0700

    modified content

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,21 +8,32 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr != NULL && new_size > 0)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		return (ptr);
+	}
+	if (new_size < old_size)
+	{
+		new_size = old_size;
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		ptr = malloc(new_size);
+		return (ptr);
 	}
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
-	return (ptr);
-}
+	return (ptr); 
+}    
