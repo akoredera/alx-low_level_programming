8b76832b057eb10361ffaf49d6519299a0afc12a
akoredera <akoredera@gmail.com>
2023-11-06 05:07:43 -0800

    pushed dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - details of dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
