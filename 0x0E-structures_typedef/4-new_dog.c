716b4cc9250393c50498d0c52ba973e570724615
akoredera <akoredera@gmail.com>
2023-11-06 08:00:52 -0800

    pushed 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - new dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+	int i, name_len, owner_len = name_len = 0;
+
+	for (i = 0; name[i] != '\0'; i++)
+		name_len++;
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_len++;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+	doggy->name = malloc(name_len * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < name_len; i++)
+		doggy->name[i] = name[i];
+	doggy->age = age;
+	doggy->owner = malloc(owner_len * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < owner_len; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
+}
