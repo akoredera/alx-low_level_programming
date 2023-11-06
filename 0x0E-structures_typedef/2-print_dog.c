ddda8c3e2613ba61ce2fe1136177ccc92ba078a7
akoredera <akoredera@gmail.com>
2023-11-06 06:47:41 -0800

    pushed 2-print_dog.c file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - print dog
+ * @d: dog struct variable
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else if (d->age == 0)
+			printf("Age: (nil)");
+		else if (d->owner == 0)
+			printf("Owner: (nil)");
+		else
+		{
+			printf("Name: %s", d->name);
+			printf("\nAge: %f", d->age);
+			printf("\nOwner: %s\n", d->owner);
+		}
+	}
+}
