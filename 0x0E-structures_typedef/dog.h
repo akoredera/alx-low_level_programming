16a67dd99cb704fbe543da68be1a28fc006a3b96
akoredera <akoredera@gmail.com>
2023-11-07 12:07:47 -0800

    modified file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,13 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
+};
+/*dog_t *new_dog(char *name, float age, char *owner);*/
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void free_dog(dog_t *d);
