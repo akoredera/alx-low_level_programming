231fd2325ef228f68a93f2a5635428941606a645
akoredera <akoredera@gmail.com>
2023-11-08 05:52:00 -0800

    pushed 1-array_iterator.c file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - array iterator
+ * @array: array
+ * @size: array size
+ * @action: function pointer
+ * Return: NULL
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
