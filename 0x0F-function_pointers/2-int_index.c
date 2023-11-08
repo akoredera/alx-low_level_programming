fb34252514b28aee461e38367afe42e08ffe3a74
akoredera <akoredera@gmail.com>
2023-11-08 06:39:55 -0800

    pushed 2-int_index.c file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+/**
+ * cmp - compare if not zero
+ * @a: integer
+ * Return: a if it is integer, otherwise -1
+ */
+int cmp(int a)
+{
+	if (a != 0)
+		return (a);
+	else
+		return (-1);
+}
+/**
+ * int_index - return index of array
+ * @array: array
+ * @size: array size
+ * @cmp: function pointer for compare if not zero
+ * Return: success (i) otherwise -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	if (cmp(array[i]))
+		return (i);
+	return (i);
+}
