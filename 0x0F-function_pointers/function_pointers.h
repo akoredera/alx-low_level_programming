b77f07750653f19a3c9789bf4cb07b31253e2a53
akoredera <akoredera@gmail.com>
2023-11-08 05:53:46 -0800

    added functions prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
+#include <stddef.h>
 #ifndef function_pointers_H
 #define function_pointers_H
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 #endif
