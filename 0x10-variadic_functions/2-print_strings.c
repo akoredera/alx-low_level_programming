d8734255ecd640f499815bf117e52d4f8d8577f5
akoredera <akoredera@gmail.com>
2023-11-09 07:26:38 -0800

    modifed temp_str is null condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,12 +18,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		temp_str = va_arg(str, char *);
-		if ((i + 1) == n)
+		if ((i + 1) == n && temp_str)
 			printf("%s", temp_str);
-		else if (!(separator[0]))
+		if ((i + 1) == n && !(temp_str))
+			printf("(nil)");
+		else if (!(separator))
 			printf("%s", temp_str);
 		else if (!(temp_str))
-			printf("(nil)");
+			printf("(nil)%s", separator);
 		else
 				printf("%s%s", temp_str, separator);
 	}
