f85047f41177be63718fac274491da6d9f52615f
akoredera <akoredera@gmail.com>
2023-11-09 06:43:35 -0800

    modified string null condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,17 +18,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		temp_str = va_arg(str, char *);
-		if (!(temp_str))
-			printf("nil");
+		if ((i + 1) == n)
+			printf("%s", temp_str);
+		else if (!(separator))
+			printf("%s", temp_str);
+		else if (!(temp_str))
+			printf("(nil)");
 		else
-		{
-			if ((i + 1) == n)
-				printf("%s", temp_str);
-			else if (!(separator))
-				printf("%s", temp_str);
-			else
 				printf("%s%s", temp_str, separator);
-		}
 	}
 	va_end(str);
 	printf("\n");
