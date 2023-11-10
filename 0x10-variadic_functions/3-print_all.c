447046c5fad1597925547fdb8698d70f5141c0cf
akoredera <akoredera@gmail.com>
2023-11-10 07:19:42 -0800

    modified the printf

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,6 @@
  * print_all - print anything
  * @format: pointer to a string of format
  * @...: argument list
- * Return: null
  */
 void print_all(const char * const format, ...)
 {
@@ -46,6 +45,6 @@ void print_all(const char * const format, ...)
 		}
 		format_ptr++;
 	}
-	printf("\n");
 	va_end(my_arg);
+	printf("\n");
 }
