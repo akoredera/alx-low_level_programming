8da00a128a8ba001fb80eaefc5b374ddb1b91d9f
akoredera <akoredera@gmail.com>
2023-11-10 06:13:41 -0800

    pushed 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - print anything
+ * @format: pointer to a string of format
+ * @...: argument list
+ * Return: null
+ */
+void print_all(const char * const format, ...)
+{
+	va_list my_arg;
+	char *format_ptr = (char *)format;
+	char *s;
+
+	va_start(my_arg, format);
+	while (*format_ptr)
+	{
+		switch (*format_ptr)
+		{
+			case 'c':
+				putchar(va_arg(my_arg, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(my_arg, double));
+				break;
+				case 'i':
+				printf("%i", va_arg(my_arg, int));
+				break;
+			case 's':
+				s = va_arg(my_arg, char *);
+				if (!(s))
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				break;
+		}
+		if (*(format_ptr + 1) != '\0')
+		{
+			switch (*(format_ptr))
+			{
+				case 'c':
+					printf(", ");
+					break;
+				case 'f':
+					printf(", ");
+					break;
+				case 's':
+					printf(", ");
+					break;
+				case 'i':
+					printf(", ");
+					break;
+				default:
+					break;
+			}
+		}
+		format_ptr++;
+	}
+	printf("\n");
+	va_end(my_arg);
+}
