3a6a47253af144b882177ae847e4ef1912cd5b2a
akoredera <akoredera@gmail.com>
2023-11-16 11:59:37 -0800

    Add: lists file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef list_H
+#define list_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+int len_str(char * str);
+char *str_result(char *str);
+size_t print_list(const list_t *h);
+#endif
