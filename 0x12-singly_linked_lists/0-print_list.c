37e65733eaedc82ab997eadd536ec36b6f6edc63
akoredera <akoredera@gmail.com>
2023-11-17 08:11:04 -0800

    modified 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,30 +16,11 @@ size_t print_list(const list_t *h)
 	while (ptr != NULL)
 	{
 		count++;
-		printf("[%u] %s\n", len_str(ptr->str), str_result(ptr->str));
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", ptr->len, ptr->str);
 		ptr = ptr->next;
 	}
 	return (count);
-}
-/**
- * len_str - return length of str
- * @str: string value
- * Return: success length otherwise 0
- */
-unsigned int len_str(char *str)
-{
-	if (str == NULL)
-		return (0);
-	return (strlen(str));
-}
-/**
- * str_result - return string
- * @str: string value
- * Return: (success) str, otherwise nil
- */
-char *str_result(char *str)
-{
-	if (str == NULL)
-		return ("(nil)");
-	return (str);
 }
