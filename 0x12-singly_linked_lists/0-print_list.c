e38c612e5b86bb95391a1491a006371e669e19a5
akoredera <akoredera@gmail.com>
2023-11-17 08:19:47 -0800

    modified 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@ size_t print_list(const list_t *h)
 	unsigned int count = 0;
 
 	if (h == NULL)
-		return (0);
+		return(0);
 	while (h != NULL)
 	{
-		count++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+		count++;
 		h = h->next;
 	}
 	return (count);
