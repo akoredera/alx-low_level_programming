b0d1557bf9c5f89bebfd70229ac3d80e3dd83dbd
akoredera <akoredera@gmail.com>
2023-11-17 03:48:55 -0800

    modified 4-free_list file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,12 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	if (head == NULL)
-		free(head);
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
-	free(temp);
+	free(head);
 
 }
