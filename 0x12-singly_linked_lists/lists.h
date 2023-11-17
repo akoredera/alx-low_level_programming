1e9716e2ca3499c39c4cb13bd4fc10f655eee365
akoredera <akoredera@gmail.com>
2023-11-17 08:23:45 -0800

    modified lists file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,10 +19,8 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 list_t *add_node_end(list_t **head, const char *str);
-unsigned int len_str(char * str);
 void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
-char *str_result(char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
