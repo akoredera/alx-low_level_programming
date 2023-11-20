a72b7c136ae4da87587e11e6dc69d2ff8c370529
akoredera <akoredera@gmail.com>
2023-11-20 09:19:45 -0800

    modified: 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
- * free_listint - free listint_t list
+ * free_listint2 - free listint_t list
  * @head: address
  * Return: NULL
  */
 void free_listint2(listint_t **head)
 {
- listint_t *temp;
+	listint_t *temp;
 
-        while (*head != NULL)
-        {
-                temp = (*head)->next;
-                free(*head);
-                *head = temp;
-        }
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
 	free(*head);
 	*head = NULL;
 }
