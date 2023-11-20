209f2bbaba687953dc7a60b6abec90cf6b5d379d
akoredera <akoredera@gmail.com>
2023-11-20 09:16:33 -0800

    modified: 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,6 +6,14 @@
  */
 void free_listint2(listint_t **head)
 {
+ listint_t *temp;
+
+        while (*head != NULL)
+        {
+                temp = (*head)->next;
+                free(*head);
+                *head = temp;
+        }
 	free(*head);
 	*head = NULL;
 }
