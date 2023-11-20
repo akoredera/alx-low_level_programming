bbd0be5d4bc217089f989f056dc0bf5dd2f0c10a
akoredera <akoredera@gmail.com>
2023-11-20 06:48:38 -0800

    add 4-free_listint.c file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free listint_t list
+ * @head: address
+ * Return: NULL
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
