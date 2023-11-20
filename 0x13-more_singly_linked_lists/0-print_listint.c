1c377f34183f8d1024d3ac00df21d7d67ca5a4d3
akoredera <akoredera@gmail.com>
2023-11-20 03:30:07 -0800

    Add 0-print_listint.c file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - print list of integer
+ * @h: contain address of node
+ * Return: number of node
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count;
+
+	if (h == NULL)
+		return (0);
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
