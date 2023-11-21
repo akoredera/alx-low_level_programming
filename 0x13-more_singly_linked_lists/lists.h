c40bc8ddd9d41913a4b9d1f42d4659bde7a2487e
akoredera <akoredera@gmail.com>
2023-11-21 06:36:30 -0800

    update lists file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t listint_len(const listint_t *h);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
