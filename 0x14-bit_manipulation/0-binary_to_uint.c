ce2d6228980e95aaf1cb967b4f3511cefc973c83
akoredera <akoredera@gmail.com>
2023-11-23 08:45:40 -0800

    modified 0-binary_to_uint.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 			dec = (dec << 1) | 1;
 		else if (b[i] == '0')
 			dec <<= 1;
-		else if (b[i] != '0' && b[i] != '1')
+		else if (b[i] != '0' || b[i] != '1')
 			return (0);
 	}
 
