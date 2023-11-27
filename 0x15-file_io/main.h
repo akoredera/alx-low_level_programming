3b5305f9b98427ecccc7fb3d7f126942371ce308
akoredera <akoredera@gmail.com>
2023-11-27 09:20:22 -0800

    update main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
