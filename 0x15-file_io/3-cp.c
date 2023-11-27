791a6f90cf9df3825cef7a53b8ef57b8d6e2e154
akoredera <akoredera@gmail.com>
2023-11-27 14:36:03 -0800

    modify 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,26 +5,22 @@
 void cp_file_from_and_file_to(const char *filename1, const char *filename2);
 /**
  * main - check the code
- *
+ * @ac: number of arguement
+ * @av: string arguments
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-        if (ac != 3)
-        {
-                dprintf(2, "Usage: cp file_from file_to\n");
-                exit(98);
-        }
-        cp_file_from_and_file_to(av[1], av[2]);
-        return (0);
-}
-
-
-void cp_file_from_and_file_to(const char *filename1, const char *filename2)
-{
 	int fd1, fd2, read_size;
-	char *ch = malloc(1024);
+	char *ch = malloc(1024), *filename1, *filename2;
 
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(98);
+	}
+	filename1 = av[1];
+	filename2 = av[2];
 	fd1 = open(filename1, O_RDONLY);
 	if (fd1 == -1)
 	{
@@ -51,4 +47,5 @@ void cp_file_from_and_file_to(const char *filename1, const char *filename2)
 		dprintf(2, "Can't close fd %d", fd2);
 		exit(100);
 	}
+	return (0);
 }
