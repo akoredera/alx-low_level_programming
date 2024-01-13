af07ca9e334506b9a83f0efb5c9428ab2695b89a
akoredera <akoredera@gmail.com>
2024-01-13 14:43:32 -0800

    modified 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,8 +11,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *temp2, *new_ptr;
 	unsigned int count = 1;
 
-	if (*h == NULL)
-		return (NULL);
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
