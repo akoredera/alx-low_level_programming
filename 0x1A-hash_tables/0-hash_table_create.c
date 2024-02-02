fe001e2af59d12f34e6e1ad248b8a4878870a133
akoredera <akoredera@gmail.com>
2024-02-02 05:43:32 -0800

    modified 0-hash_tables file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (size == NULL)
+		return NULL
+
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
