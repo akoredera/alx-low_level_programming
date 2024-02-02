2a0d241436421fc7731be19ec67f9e8872351143
akoredera <akoredera@gmail.com>
2024-02-02 12:41:19 -0800

    modified 3-hash_table_set file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,24 +8,41 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int key_pos;
-	hash_node_t *current_node, *new_node;
 
-	key_pos = key_index((const unsigned char *)key, ht->size);
-	current_node = ht->array[key_pos];
-	if (current_node == NULL)
+	unsigned long int key_pos, i;
+	hash_node_t *new_node;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (strcmp(ht->array[key_pos]->key, key) == 0)
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+	key_pos = key_index((const unsigned char *)key, ht->size);
+	for (i = key_pos; ht->array[i]; i++)
 	{
-		free(ht->array[key_pos]->value);
-		ht->array[key_pos]->value = (char *)value;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = new_value;
+			return (1);
+		}
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(new_value);
 		return (0);
+	}
 	new_node->key = strdup(key);
-	new_node->value = (char *)value;
+	if (new_node->key == NULL)
+	{
+		free(new_value);
+		free(new_node);
+		return (0);
+	}
+	new_node->value = new_value;
 	new_node->next = ht->array[key_pos];
 	ht->array[key_pos] = new_node;
 
